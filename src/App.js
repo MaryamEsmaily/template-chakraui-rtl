0ae677f280d1df7dcc59dd1300910b27c73895f8
Maryam <89831604+MaryamEsmaily@users.noreply.github.com>
2022-12-07 13:05:46 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Box, Container } from "@chakra-ui/react";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container maxW="full" px={3} mt={5}>
+        <Box>صفحه نخست</Box>
+      </Container>
     </div>
   );
 }
---
+++ src/container/layout/AppLayout/AppLayoutMain/style.js
@@ -0,0 +1,33 @@
+import isValidHTMLProp from "@emotion/is-prop-valid";
+import { chakra, shouldForwardProp } from "@chakra-ui/react";
+import appLayoutConfig from "config/appLayout.config";
+
+const Main = chakra("main", {
+  shouldForwardProp: (prop) => {
+    const isChakraProp = !shouldForwardProp(prop);
+    if (isChakraProp) return false;
+    const isValidProp = isValidHTMLProp(prop);
+    if (isValidProp) return true;
+    return ["sample"].includes(prop);
+  },
+  baseStyle: ({ lg, open, theme }) => {
+    const { easing, duration } = theme.transition;
+    return {
+      transition: [easing["ease-in-out"], duration["fast"]].join(" "),
+      mr: 10 + "px",
+      w: `calc(100% - ${
+        (open
+          ? appLayoutConfig.PrimarySideOpenWidth
+          : appLayoutConfig.PrimarySideCloseWidth) + 50
+      }px)`,
+      overflow: "hidden",
+      ml: `${
+        open
+          ? appLayoutConfig.PrimarySideOpenWidth
+          : appLayoutConfig.PrimarySideCloseWidth
+      }px`,
+    };
+  },
+});
+
+export { Main };
